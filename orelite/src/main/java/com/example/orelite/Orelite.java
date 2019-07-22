8906df56596983a0ab46f3b69e741311632709a2
hempflower <1993996310@qq.com>
2019-07-22 11:25:42 +0800

    ORMLITE

---
+++ orelite/src/main/java/com/example/orelite/Orelite.java
@@ -0,0 +1,17 @@
+package com.example.orelite;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Orelite extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+        //本脚手架添加了ORELITE依赖，你可以在查询有个ORELITE的具体用法
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
